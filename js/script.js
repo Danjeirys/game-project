ac6c057d513470b2b295d0e7cb306c2ca2b140f7
Jerry Martinez <jaysavageg@gmail.com>
2022-11-04 09:19:40 -0400

    working

--- js/script.js
+++ js/script.js
@@ -78,8 +78,8 @@ function startGame () {
     console.log (yourSum)
     document.getElementById ('hit').addEventListener ('click', hit) // calls on the 'hit' function giving the user a new card
     document.getElementById ('stay').addEventListener ('click', stay) // calls on the stay function letting the user end their turn with current total sum
-    // document.getElementById ('reset').addEventListener ('click', resetGame)
-    document.getElementById ('life').addEventListener ('click', betLife)
+    document.getElementById ('restart').addEventListener ('click', resetGame)
+    // document.getElementById ('life').addEventListener ('click', betLife)
 }
 
 // giving the 'hit' button functionality
@@ -158,8 +158,34 @@ function stay () {
 }
 
 function resetGame () {
-    
-}
+    hidden = deck.pop ()
+    dealerSum += getValue (hidden)
+    dealerAceCount += checkAce (hidden)
+
+    while (dealerSum < 17) {
+        let cardImg = document.createElement ('img') // created an <img> tag
+        let card = deck.pop () // got card from the deck
+        cardImg.src = './cards/' + card + '.png' // set the src for the img tag
+        dealerSum += getValue (card) // increments the dealer sum
+        dealerAceCount += checkAce (card) // checked how many aces he has using the checkAce function
+        document.getElementById ('dealer-cards').append (cardImg) // appended the <img> tag to the dealer-cards
+    }
+    console.log (dealerSum)
+
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement ('img')
+        let card = deck.pop () 
+        cardImg.src = './cards/' + card + '.png' 
+        yourSum += getValue (card) 
+        yourAceCount += checkAce (card) 
+        document.getElementById('user-cards').append(cardImg) 
+    }
+
+    document.getElementById ('dealer-sum').innerText = dealerSum 
+    document.getElementById ('user-sum').innerText = yourSum 
+
+    window.location.reload()
+} 
 
 function getValue (card) {
     let data = card.split ('-') // by calling split we calling the values ['4', 'C']
